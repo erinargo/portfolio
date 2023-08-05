28b61688ac05c2129bb325335213c5ece65e9b85
Erin Argo <137727898+erinargo@users.noreply.github.com>
2023-08-05 12:39:07 -0400

    Update index.js

--- src/components/portfolio/index.js
+++ src/components/portfolio/index.js
@@ -11,25 +11,17 @@ const Portfolio = () => {
                         Portfolio - ReactJS
                         <iframe src={ '/' } />
                     </a>
-                    <a href={ 'https://www.cardmedaddy.com' }>
-                        CardMe
-                        <iframe src={ 'https://www.cardmedaddy.com' } />
-                    </a>
-                    <a href={ 'https://www.artbindr.com' }>
-                        ArtBindr
-                        <iframe src={ 'https://www.artbindr.com' } />
-                    </a>
                     <a href={ 'https://github.com/erinargo/C-Card-Game---War' }>
                         C++ Card Game - War
-                        <iframe src={ 'https://github.com/erinargo/C-Card-Game---War' } />
+                        GitHub doesn't allow for IFrames, please click me <3
                     </a>
                     <a href={ 'https://github.com/erinargo/CSC471' }>
                         CSC 471
-                        <iframe src={ 'https://github.com/erinargo/CSC471' } />
+                        GitHub doesn't allow for IFrames, please click me <3
                     </a>
                     <a href={ 'https://github.com/erinargo/OnlyPets' }>
                         OnlyPets
-                        <iframe src={ 'https://github.com/erinargo/OnlyPets' } />
+                        GitHub doesn't allow for IFrames, please click me <3
                     </a>
                 </div>
             </span>
