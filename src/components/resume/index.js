76e94f6339362968e92434968873c75c8134e9b0
erinargo <d_argo@uncg.edu>
2024-05-24 15:42:00 -0400

    switched pdf doc to object

--- src/components/resume/index.js
+++ src/components/resume/index.js
@@ -11,15 +11,16 @@ pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/$
 const Resume = () => {
     return (
         <>
-            <Nav />
-            <div className={'doc-wrapper'}>
-                <embed src="./Erin resume.pdf" width="500" height="375"
-                       type="application/pdf"/>
-            </div>
+            <Nav/>
+
+            <object className="pdf"
+                    data=
+                        "./Erin resume.pdf">
+            </object>
 
             <a href={'https://www.dropbox.com/scl/fi/m48mv0hgpoyzze0g9xxeo/Erin-Resume.pdf?rlkey=cesrx892smokt42ozruz1plus&st=lfodj4ly&dl=1'}>
-                <FontAwesomeIcon className={ 'download' } icon={ faDownload }></FontAwesomeIcon>
-            </a> 
+                <FontAwesomeIcon className={'download'} icon={faDownload}></FontAwesomeIcon>
+            </a>
         </>
     );
 }
