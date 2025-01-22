d3bf74fde771e24ddc58af5951cda47c98c40a55
erinargo <d_argo@uncg.edu>
2025-01-22 13:48:26 -0500

    updated a few things

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -9,13 +9,17 @@ const About = () => {
         <>
             <Nav />
             <span className={ 'about' }>
-                Hey! My name is Erin. I recently graduated with a bachelors degree in Computer Science from the University of North Carolina at Greensboro.
-                I have many passions in life (not limited to, but, most certainly including my cats) but, most recently, Game Development
-                has taken me by storm! Currently, I'm working on a game project in VR for a Virtual Fashion Show and I have plans for some personal projects that I expect
-                to finish this year!
+                Hi! My name is Erin. I graduated from The University of North Carolina at Greensboro in 2024 with my Bachelors of Science in 
+                Computer Science where, as a student, 
+                I worked as a research assistant under Dr. Regis Kopper. Since then, I have been an avid researcher and PhD student of 
+                Human-Computer Interaction working under Dr. Jeronimo Grandi at Augusta University in Augusta Georgia. 
+                My active interest is in researching, designing, and developing 3D User-Interfaces. 
+                My current work involves active design in Unity using C\# for Augmented Reality applications.
                 <br/><br/>
-                However, most of my experience lies in UI/UX design and Fullstack Development. In that area I have 8 years of professional and unprofessional
-                experience as a junior developer and researcher. Some of my most notable projects include FirstModulAR, ArtBindr, and CardMe.
+                Most of my experience lies in UI/UX Design, Game Development, and Fullstack Engineering. In that area I have 8 years of professional and unprofessional
+                experience as a junior developer and researcher. Some of my most notable projects include 
+                <a href={}>FirstModulAR (FMAR)</a>, 
+                <a href={}>Asymmetric Cross-Reality Collaboration (ACCR)</a>, and <a href={}>StyleVeRse (StyleVR)</a>.
                 Feel free to explore my portfolio to see some of my work. If you'd like to collaborate or just chat,
                 you can reach me at <a href={ 'mailto:erinjamieargo@gmail.com' }> erinjamieargo@gmail.com </a> or connect with me on
                 <a href={ 'https://www.linkedin.com/in/erin-argo-9b4052281/' }> LinkedIn</a>. Additionally, I have a public repository of most of my work that
--- src/components/portfolio/index.js
+++ src/components/portfolio/index.js
@@ -6,19 +6,28 @@ const Portfolio = () => {
         <>
             <Nav />
             <span className={ 'pages-wrapper' }>
-                <div className={ 'pages' }>
-                    <a href={ '/' }>
+                <div className={'pages'}>
+                    <a href={'/'}>
                         Portfolio - ReactJS
-                        <iframe src={ '/portfolio' } />
+                        <iframe src={'/portfolio'}/>
+                    </a>
+                    
+                    <a href={'/'}>
+                        Asymmetric Cross-Reality Collaboration (ACCR)
+                        <img 
+                            src={} 
+                            alt={} />
                     </a>
 
                     <a>
-                        FirstModulAR
-                        <img src={ 'https://www.dropbox.com/scl/fi/7fop0amcjxr54lxtmbhra/fmar.png?rlkey=evrp612iz0j2xgg9l47wsq4cm&st=unee4pwl&dl=1' }  alt={ 'FMAR Core Concept' }/>
+                        FirstModulAR (FMAR)
+                        <img
+                            src={'https://www.dropbox.com/scl/fi/7fop0amcjxr54lxtmbhra/fmar.png?rlkey=evrp612iz0j2xgg9l47wsq4cm&st=unee4pwl&dl=1'}
+                            alt={'FMAR Core Concept'}/>
                     </a>
 
-                    <a href={ 'https://irlab.uncg.edu:8081/erinargo/vr-fashion-show' }>
-                        StyleVeRse <br/>
+                    <a href={'https://irlab.uncg.edu:8081/erinargo/vr-fashion-show'}>
+                        StyleVeRse (StyleVR) <br/>
                         No Graphic Representation at this time
                     </a>
 
